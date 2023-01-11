f666100776301ddd1faad9134833ef67e9fc8f63
Oliver Lillberg <44681414+xDeZex@users.noreply.github.com>
2023-01-11 17:27:39 +0100

    changed ip

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 function recommend() {
     var username = document.getElementById('username').value;
-    fetch("http://localhost:3333/test?name=" + username).then(function(response) {
+    fetch("90.230.66.36:3333/test?name=" + username).then(function(response) {
       return response.json();
     }).then(function(data) {
       document.getElementById('rec').innerHTML = data;
